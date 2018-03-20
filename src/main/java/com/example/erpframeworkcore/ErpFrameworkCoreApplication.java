8f3ebe2289538a81519ded1d2971aaa960d440cd
贺兰沁焱 <1123569385@qq.com>
2018-03-20 21:33:43 +0800

    构建了项目
    构建了项目

---
+++ src/main/java/com/example/erpframeworkcore/ErpFrameworkCoreApplication.java
@@ -0,0 +1,12 @@
+package com.example.erpframeworkcore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErpFrameworkCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ErpFrameworkCoreApplication.class, args);
+	}
+}
---
+++ src/test/java/com/example/erpframeworkcore/ErpFrameworkCoreApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.erpframeworkcore;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ErpFrameworkCoreApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
